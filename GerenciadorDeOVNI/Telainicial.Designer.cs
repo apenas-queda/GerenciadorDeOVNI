951b5653b3cdc43047d6fbd2b84845d8400a08cb
Flávio Henrique Motta Calazans <fhmc3@outlook.com>
2025-08-22 21:54:35 -0300

    Initial commit

---
+++ GerenciadorDeOVNI/Telainicial.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace GerenciadorDeOVNI
+{
+    partial class Telainicial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Telainicial));
+            this.lblTripulantes = new System.Windows.Forms.Label();
+            this.txbTripulantes = new System.Windows.Forms.TextBox();
+            this.lblAbduzidos = new System.Windows.Forms.Label();
+            this.txbAbduzidos = new System.Windows.Forms.TextBox();
+            this.lblPlaneta = new System.Windows.Forms.Label();
+            this.cmbPlanetas = new System.Windows.Forms.ComboBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTripulantes
+            // 
+            this.lblTripulantes.AutoSize = true;
+            this.lblTripulantes.Location = new System.Drawing.Point(201, 150);
+            this.lblTripulantes.Name = "lblTripulantes";
+            this.lblTripulantes.Size = new System.Drawing.Size(113, 13);
+            this.lblTripulantes.TabIndex = 0;
+            this.lblTripulantes.Text = "Máximo de Tripulantes";
+            // 
+            // txbTripulantes
+            // 
+            this.txbTripulantes.Location = new System.Drawing.Point(320, 147);
+            this.txbTripulantes.Name = "txbTripulantes";
+            this.txbTripulantes.Size = new System.Drawing.Size(100, 20);
+            this.txbTripulantes.TabIndex = 1;
+            // 
+            // lblAbduzidos
+            // 
+            this.lblAbduzidos.AutoSize = true;
+            this.lblAbduzidos.Location = new System.Drawing.Point(201, 173);
+            this.lblAbduzidos.Name = "lblAbduzidos";
+            this.lblAbduzidos.Size = new System.Drawing.Size(110, 13);
+            this.lblAbduzidos.TabIndex = 2;
+            this.lblAbduzidos.Text = "Máximo de Abduzidos";
+            // 
+            // txbAbduzidos
+            // 
+            this.txbAbduzidos.Location = new System.Drawing.Point(320, 173);
+            this.txbAbduzidos.Name = "txbAbduzidos";
+            this.txbAbduzidos.Size = new System.Drawing.Size(100, 20);
+            this.txbAbduzidos.TabIndex = 3;
+            // 
+            // lblPlaneta
+            // 
+            this.lblPlaneta.AutoSize = true;
+            this.lblPlaneta.Location = new System.Drawing.Point(201, 198);
+            this.lblPlaneta.Name = "lblPlaneta";
+            this.lblPlaneta.Size = new System.Drawing.Size(94, 13);
+            this.lblPlaneta.TabIndex = 4;
+            this.lblPlaneta.Text = "Planeta de Origem";
+            // 
+            // cmbPlanetas
+            // 
+            this.cmbPlanetas.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPlanetas.FormattingEnabled = true;
+            this.cmbPlanetas.Location = new System.Drawing.Point(320, 199);
+            this.cmbPlanetas.Name = "cmbPlanetas";
+            this.cmbPlanetas.Size = new System.Drawing.Size(100, 21);
+            this.cmbPlanetas.TabIndex = 5;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(204, 253);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(216, 45);
+            this.btnIniciar.TabIndex = 6;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("EngraversGothic BT", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(161, 83);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(299, 31);
+            this.lblTitulo.TabIndex = 7;
+            this.lblTitulo.Text = "UFO MANAGER 3000";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::GerenciadorDeOVNI.Properties.Resources.Goodstuff_No_Nonsense_Free_Space_Alien_obduction_1024__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(26, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(95, 76);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Telainicial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 615);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.cmbPlanetas);
+            this.Controls.Add(this.lblPlaneta);
+            this.Controls.Add(this.txbAbduzidos);
+            this.Controls.Add(this.lblAbduzidos);
+            this.Controls.Add(this.txbTripulantes);
+            this.Controls.Add(this.lblTripulantes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Telainicial";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inicializador";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTripulantes;
+        private System.Windows.Forms.TextBox txbTripulantes;
+        private System.Windows.Forms.Label lblAbduzidos;
+        private System.Windows.Forms.TextBox txbAbduzidos;
+        private System.Windows.Forms.Label lblPlaneta;
+        private System.Windows.Forms.ComboBox cmbPlanetas;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# GerenciadorDeOVNI
+
